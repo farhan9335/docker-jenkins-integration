1ddebee9681ee6f93c0d8776dca17028f62f2c88
unknown <khan.farhan1985@gmail.com>
2023-10-24 09:33:47 +0530

    [Initial Commit]

---
+++ src/main/java/com/example/mypack/DockerJenkinsIntegrationApplication.java
@@ -0,0 +1,28 @@
+package com.example.mypack;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerJenkinsIntegrationApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(DockerJenkinsIntegrationApplication.class);
+	
+	public static void main(String[] args) {
+		logger.info("<---------Inside main method--------------------->");
+		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+		logger.info("<--------Main method completed------------------->");
+	}
+	
+	@GetMapping(path = "/welcome")
+	public String welComeMessage() {
+		logger.info("<---------Inside welComeMessage method--------------------->");
+		return "Welcome first docker jenkins integration";
+	}
+
+}
